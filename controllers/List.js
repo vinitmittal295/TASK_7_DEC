68d8b48b01a254c504ae1d4394068172b9987bda
vinit mittal <vinitmittal295@gmail.com>
2024-12-17 14:21:03 +0530

    added

--- controllers/List.js
+++ controllers/List.js
@@ -14,4 +14,27 @@ exports.createList=async(req,res)=>{
 
 exports.getAllList=async(req,res)=>{
     const list =await List.find()
+    res.status(200).json(list)
+}
+exports.singleList=async(req,res)=>{
+    const id=req.params.id
+    const list=await List.findById(id)
+    if(!list){
+        return res.status(400).json({message:"List not found"})
+    }
+    res.status(200).json(list)
+}
+exports.updateList=async(req,res)=>{
+    const id=req.params.id
+    const {data}=req.body
+    const list=await List.findByIdAndUpdate(id,data)
+    if(!list){
+        return res.status(400).json({message:"List not found"})
+    }
+    res.status(200).json(list)
+}
+exports.deleteList=async(req,res)=>{
+    const id=req.params.id
+    const list=await List.findByIdAndDelete(id)
+    res.status(200).json(list)
 }
